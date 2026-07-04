849be2ec73a083bf60dc844189212608a2bd0322
Tomson <gifttomp9@gmail.com>
2026-07-04 21:47:22 +0530

    rotating shape/cursor

--- Gameengine.c
+++ Gameengine.c
@@ -200,32 +200,84 @@ void get_cursor(int shape_no){
     }
 }
 
+ void rotate(){
+    int temp[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            temp[i][j]=cursor.pos[j][3-i];
+        }
+    }
+   
+     while(temp[0][0]+temp[1][0]+temp[2][0]+temp[3][0]==0){
+         for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(j!=3)
+           temp[i][j]= temp[i][j+1];
+           else
+           temp[i][j]= 0;
+        }
+    }
+    }
+     while(temp[0][0]+temp[0][1]+temp[0][2]+temp[0][3]==0){
+         for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(i!=3)
+           temp[i][j]= temp[i+1][j];
+           else
+           temp[i][j]= 0;
+        }
+    }
+    }
+     for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+           cursor.pos[i][j]= temp[i][j];
+        }
+    }
+    
+ }
 
-struct board* print_board(struct board* last){
+ void check_board(){
+    int temp[4][4],stop=0;
+    do{  
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+          if(cursor.pos[i][j]+temp[i][j]==2){
+            stop=1;
+          }
+        }
+    }
+}while(stop==1)
+ }
+
+
+void print_board(struct board* last){
     system("cls");
-    struct board* current=last;
     int i,j,k;
     for(i=0;i<16;i++){
         printf("||");
         for(j=0;j<8;j++){
-            if(current->column[j]==0)
+            if(last->column[j]==0)
             printf(" ");
             else
             printf("*");
         }
         printf("||    ");
         if((i<11)&&(i>0)){
-          if((i==1)&&(i==10)){
+          if((i==1)||(i==10)){
             printf("==========");
           }
           else{
             k=4-(i/2);
+            printf("|");
             for(j=0;j<4;j++){
+                
                if(cursor.pos[k][j]==0)
                printf("  ");
                else
                printf("##");
+               
             }  
+            printf("|");
         }          
         }
 
@@ -233,7 +285,7 @@ struct board* print_board(struct board* last){
         printf("\n");
     }
 
- return last;   
+
 }
 
 
@@ -243,8 +295,14 @@ void game(){
     int temp;
     set_shapes();
         get_cursor(rand_shape());
-        last=print_board(last);
+        print_board(last);
         temp=getch();
+        while(temp=='r'){
+        rotate();
+        print_board(last);
+        temp=getch();
+        }
+
         END(last);
        
 }
